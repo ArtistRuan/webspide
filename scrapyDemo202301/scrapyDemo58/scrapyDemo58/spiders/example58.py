a7cf5cb8b79e82933661a3050074811666c0055f
ArtistRuan <m2ruan@126.com>
2023-02-02 22:59:24 +0800

    20230202 scrapy 58

--- scrapyDemo202301/scrapyDemo58/scrapyDemo58/spiders/example58.py
+++ scrapyDemo202301/scrapyDemo58/scrapyDemo58/spiders/example58.py
@@ -4,7 +4,18 @@ import scrapy
 class Example58Spider(scrapy.Spider):
     name = 'example58'
     allowed_domains = ['sz.58.com']
-    start_urls = ['http://sz.58.com/']
+    start_urls = ['https://sz.58.com/ershoufang']
 
     def parse(self, response):
-        pass
+
+        print("执行了这里")
+
+        house_name_list = response.xpath('//*[@id="esfMain"]/section/section[3]/section[1]/section[2]//@title').extract()
+        # house_name_list = response.xpath('//*[@id="esfMain"]/section/section[3]/section[1]/section[2]/div[1]/a/div[2]/div[1]/div[1]/h3/@title').extract()
+
+        print("类型", type(house_name_list))
+        print("获取到的数据",house_name_list)
+        # for i in range(len(house_name_list)):
+        #     print(i, house_name_list[i])
+
+
